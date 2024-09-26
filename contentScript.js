ed56f7769ec5d991b10d47cb155be01994994926
GitTsubasa <pwang11@binghamton.edu>
2024-09-25 22:44:42 -0400

    fixed CSP error

--- contentScript.js
+++ contentScript.js
@@ -2,7 +2,7 @@ const logoElement = document.querySelector("#guide-spacer");
 console.log(logoElement);
 console.dir(document);
 const newElement = document.createElement("div");
-newElement.innerHTML = "<h1>Hello World<h1>";
+newElement.innerHTML = "Hello World";
 if (logoElement) {
-  document.parentNode.replaceChild(newElement, logoElement);
+  logoElement.parentNode.replaceChild(newElement, logoElement);
 }
