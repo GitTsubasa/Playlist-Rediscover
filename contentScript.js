6758a8e39ffaf81246defd3126fbf1ba76125811
GitTsubasa <pwang11@binghamton.edu>
2024-10-07 17:27:52 -0400

    added snapshot

--- contentScript.js
+++ contentScript.js
@@ -1,10 +1,20 @@
 // let logoElement = document.querySelector("#logo-icon");
-let hiddenElement = document.querySelector(
-  "ytd-alert-with-button-renderer yt-formatted-string#text"
-);
-console.log(hiddenElement);
+let hiddenElement = document.querySelector("ytd-alert-with-button-renderer");
+// console.log(logoElement);
 const newElement = document.createElement("div");
 newElement.innerHTML = "Hello World";
 if (hiddenElement) {
   hiddenElement.parentNode.replaceChild(newElement, hiddenElement);
+}
+
+const nodesSnapshot = document.evaluate(
+  "//ytd-alert-with-button-renderer",
+  document,
+  null,
+  XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+  null
+);
+
+for (let i = 0; i < nodesSnapshot.snapshotLength; i++) {
+  console.log(nodesSnapshot.snapshotItem(i).textContent);
 }
