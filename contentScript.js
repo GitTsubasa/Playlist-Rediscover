91497d05fcdd45d9dba5ba61890d017c38d598b5
GitTsubasa <pwang11@binghamton.edu>
2024-10-09 20:12:03 -0400

    debugging, interact with the DOM

--- contentScript.js
+++ contentScript.js
@@ -1,33 +1,23 @@
-// let logoElement = document.querySelector("#logo-icon");
-let hiddenElement = document.querySelector("ytd-alert-with-button-renderer");
-// console.log(logoElement);
-const newElement = document.createElement("div");
-newElement.innerHTML = "Hello World";
-if (hiddenElement) {
-  hiddenElement.parentNode.replaceChild(newElement, hiddenElement);
-}
-
-// const nodesSnapshot = document.evaluate(
-//   "//ytd-alert-with-button-renderer",
-//   document,
-//   null,
-//   XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
-//   null
-// );
-
-// for (let i = 0; i < nodesSnapshot.snapshotLength; i++) {
-//   console.log(nodesSnapshot.snapshotItem(i).textContent);
-// }
-
+console.log("Content script loaded");
 document.addEventListener("DOMContentLoaded", function () {
-  const nodesSnapshot = document.evaluate(
-    "//ytd-alert-with-button-renderer",
-    document,
-    null,
-    XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
-    null
-  );
-  for (let i = 0; i < nodesSnapshot.snapshotLength; i++) {
-    console.log(nodesSnapshot.snapshotItem(i).textContent);
-  }
+  // // let logoElement = document.querySelector("#logo-icon");
+  // let hiddenElement = document.querySelector("ytd-alert-with-button-renderer");
+  // // console.log(logoElement);
+  // const newElement = document.createElement("div");
+  // newElement.innerHTML = "Hello World";
+  // if (hiddenElement) {
+  //   hiddenElement.parentNode.replaceChild(newElement, hiddenElement);
+  // }
+
+  // const nodesSnapshot = document.evaluate(
+  //   "//ytd-alert-with-button-renderer",
+  //   document,
+  //   null,
+  //   XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+  //   null
+  // );
+  // for (let i = 0; i < nodesSnapshot.snapshotLength; i++) {
+  //   console.log(nodesSnapshot.snapshotItem(i).textContent);
+  // }
+  console.log("DOM fully loaded and parsed");
 });
