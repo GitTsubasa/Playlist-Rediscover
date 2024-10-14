90e2bda5f8caadcc38c0a4a6a6075ccb37b74cde
GitTsubasa <pwang11@binghamton.edu>
2024-10-14 17:48:16 -0400

    mutation observer

--- contentScript.js
+++ contentScript.js
@@ -1,23 +1,40 @@
 console.log("Content script loaded");
 document.addEventListener("DOMContentLoaded", function () {
-  // // let logoElement = document.querySelector("#logo-icon");
-  // let hiddenElement = document.querySelector("ytd-alert-with-button-renderer");
-  // // console.log(logoElement);
-  // const newElement = document.createElement("div");
-  // newElement.innerHTML = "Hello World";
-  // if (hiddenElement) {
-  //   hiddenElement.parentNode.replaceChild(newElement, hiddenElement);
-  // }
+  const targetNode = document.body;
+  const config = { childList: true, subtree: true };
 
-  // const nodesSnapshot = document.evaluate(
-  //   "//ytd-alert-with-button-renderer",
-  //   document,
-  //   null,
-  //   XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
-  //   null
-  // );
-  // for (let i = 0; i < nodesSnapshot.snapshotLength; i++) {
-  //   console.log(nodesSnapshot.snapshotItem(i).textContent);
-  // }
+  const callback = function (mutationsList, observer) {
+    for (let mutation of mutationsList) {
+      if (mutation.type === "childList") {
+        let hiddenElement = document.querySelector(
+          "ytd-alert-with-button-renderer"
+        );
+        if (hiddenElement) {
+          const newElement = document.createElement("div");
+          newElement.innerHTML = "Hello World";
+          hiddenElement.parentNode.replaceChild(newElement, hiddenElement);
+          console.log("Replaced hidden element");
+
+          const nodesSnapshot = document.evaluate(
+            "//ytd-alert-with-button-renderer",
+            document,
+            null,
+            XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+            null
+          );
+          for (let i = 0; i < nodesSnapshot.snapshotLength; i++) {
+            console.log(nodesSnapshot.snapshotItem(i).textContent);
+          }
+          // Stop observing after the target element is found and replaced
+          observer.disconnect();
+          break;
+        }
+      }
+    }
+  };
+
+  const observer = new MutationObserver(callback);
+  observer.observe(targetNode, config);
+
   console.log("DOM fully loaded and parsed");
 });
