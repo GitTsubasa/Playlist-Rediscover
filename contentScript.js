4ed0994579f80b03801989b3ac12e854eb2bbc7f
GitTsubasa <pwang11@binghamton.edu>
2024-10-15 23:39:36 -0400

    started on button pressing

--- contentScript.js
+++ contentScript.js
@@ -1,40 +1,54 @@
 console.log("Content script loaded");
-document.addEventListener("DOMContentLoaded", function () {
-  const targetNode = document.body;
-  const config = { childList: true, subtree: true };
+// const testElement = document.querySelector("#logo-icon");
+// const newElement2 = document.createElement("div");
+// newElement2.innerHTML = "hihihi";
+// testElement.parentNode.replaceChild(newElement2, testElement);
+// if (testElement) {
+//   console.log("Found element");
+//   testElement.style.backgroundColor = "red";
+// }
 
-  const callback = function (mutationsList, observer) {
-    for (let mutation of mutationsList) {
-      if (mutation.type === "childList") {
-        let hiddenElement = document.querySelector(
-          "ytd-alert-with-button-renderer"
-        );
-        if (hiddenElement) {
-          const newElement = document.createElement("div");
-          newElement.innerHTML = "Hello World";
-          hiddenElement.parentNode.replaceChild(newElement, hiddenElement);
-          console.log("Replaced hidden element");
+const targetNode = document.body;
+const config = { childList: true, subtree: true };
 
-          const nodesSnapshot = document.evaluate(
-            "//ytd-alert-with-button-renderer",
-            document,
-            null,
-            XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
-            null
-          );
-          for (let i = 0; i < nodesSnapshot.snapshotLength; i++) {
-            console.log(nodesSnapshot.snapshotItem(i).textContent);
-          }
-          // Stop observing after the target element is found and replaced
-          observer.disconnect();
-          break;
-        }
-      }
+function showUnavailableVideos() {
+  const SUVButtonContainer = document.querySelector(
+    "yt-list-item-view-model-wiz__label yt-list-item-view-model-wiz__container yt-list-item-view-model-wiz__container--compact yt-list-item-view-model-wiz__container--tappable yt-list-item-view-model-wiz__container--in-popup"
+  );
+  if (SUVButtonContainer) {
+    const SUVSpanContainer = document.querySelector(
+      "yt-core-attributed-string yt-list-item-view-model-wiz__title yt-core-attributed-string--white-space-pre-wrap"
+    );
+    console.log("Found SUVButtonContainer");
+    if (SUVSpanContainer) {
+      console.log("FoundSUVSpanContainer");
+      SUVButtonContainer.click();
     }
-  };
+  } else {
+    console.log("SUVButtonContainer could not be found.");
+  }
+}
 
-  const observer = new MutationObserver(callback);
-  observer.observe(targetNode, config);
+const callback = function (mutationsList, observer) {
+  for (let mutation of mutationsList) {
+    if (mutation.type === "childList") {
+      showUnavailableVideos();
+      observer.disconnect();
+      break;
+    }
+  }
+};
 
-  console.log("DOM fully loaded and parsed");
-});
+const observer = new MutationObserver(callback);
+observer.observe(targetNode, config);
+
+// const nodesSnapshot = document.evaluate(
+//   "//ytd-alert-with-button-renderer",
+//   document,
+//   null,
+//   XPathResult.ORDERED_NODE_SNAPSHOT_TYPE,
+//   null
+// );
+// for (let i = 0; i < nodesSnapshot.snapshotLength; i++) {
+//   console.log(nodesSnapshot.snapshotItem(i).textContent);
+// }
